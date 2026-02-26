272e3bcb4aa5ee862448e25123fa112ecfa4a6ae
cburke6897 <cburke6897@gmail.com>
2026-02-26 16:55:53 -0500

    Implements page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home.jsx'
+import Home from './pages/Home.jsx'
+import Projects from './pages/Projects.jsx'
+import Experience from './pages/Experience.jsx'
+import Tools from './pages/Tools.jsx'
+import Contact from './pages/Contact.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/tools" element={<Tools />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   )
 }
